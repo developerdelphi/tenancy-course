b9327a29c220c3ff7e0191ec429d62999a631c8e
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-17 09:49:53 -0300

    Create Route Resources Environment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EnvironmentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/environments');
 });
+
+Route::resource('environments', EnvironmentController::class);
