763f5e5443c027366f26e007d7212992ec0eac17
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-18 09:32:04 -0300

    Feat: Enviroment - Order layout for sm and major md

--- resources/views/environments/index.blade.php
+++ resources/views/environments/index.blade.php
@@ -16,22 +16,32 @@ Lista de Ambientes
 @section('content')
 <div class="m-4">
     <h1 class="text-3xl h-20 font-bold">Lista de Ambientes</h1>
-    <div>
+    <div class="flex flex-col">
         @forelse ($environments as $environment )
-        <div class="flex flex-row items-center p-2 border shadow justify-between">
-            <h3 class="text-lg font-semibold p-4">{{ $environment->id }}</h3>
-            <div class="flex gap-2 items-center justify-between ">
-                <div>
-                    Database:{{ $environment->tenancy_db_name }}
+        <div
+            class="grid grid-flow-row auto-rows-max sm:grid-cols-1 gap-2 md:grid-cols-3 items-center p-2 border shadow w-full">
+            <div class="md:col-span-2 flex items-center w-full">
+                <h3 class="text-lg font-semibold p-4">{{ $environment->id }}</h3>
+                <div class="flex flex-row gap-2 text-xs">
+                    <div class="w-1/2">Database: <span class="font-semibold">{{ $environment->tenancy_db_name }}</span>
+                    </div>
+
+                    <div class="w-1/2">Criação: <span class="font-semibold">{{ $environment->created_at }}</span></div>
                 </div>
-                <div>
-                    Criação{{ $environment->created_at }}
-                </div>
-                <div class="p-2 w-40 flex flex-row justify-end gap-2">
-                    <a class="bg-gray-400 text-gray-50 font-semibold p-2 shadow rounded-lg hover:bg-gray-600"
+            </div>
+            <div class="flex flex-row justify-end">
+                <div class="grid grid-cols-3 gap-0 justify-between bg-gray-400 h-8">
+                    <a class="text-xs text-gray-50 font-semibold p-2 hover:bg-gray-600 h-8"
                         href="{{ route('environments.edit', ['environment'=>$environment->id]) }}">Editar</a>
-                    <a class="bg-gray-400 text-gray-50 font-semibold p-2 shadow rounded-lg hover:bg-gray-600"
+                    <a class="text-xs text-gray-50 font-semibold p-2 hover:bg-gray-600 h-8"
                         href="{{ route('environments.edit', ['environment'=>$environment->id]) }}">Domínio</a>
+                    <form action="{{ route('environments.destroy', ['environment'=>$environment->id]) }}" method="post"
+                        class="p-0 m-0 h-8">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"
+                            class="text-xs text-gray-50 font-semibold p-2 hover:bg-gray-600">Excluir</button>
+                    </form>
                 </div>
             </div>
         </div>
