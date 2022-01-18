d3bff5f637fffeee32943418177775b234332e67
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-18 09:37:25 -0300

    Feat: Enviroment - View Form show erros validation

--- resources/views/environments/form.blade.php
+++ resources/views/environments/form.blade.php
@@ -21,14 +21,25 @@ Lista de Ambientes
             class="block p-6 rounded-lg shadow-lg bg-white max-w-lg">
             @csrf
             <div class="form-group mb-6">
-                <input type="text" name="id"
+                <input type="text" name="id" value="{{ old('id') }}"
                     class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="environment_id" placeholder="Ambiente">
+                @if ($errors->has('id'))
+                @foreach ($errors->get('id') as $message)
+                <span class="text-red-500 font-italic text-xs px-2">{{ $message }}</span>
+                @endforeach
+                @endif
             </div>
+
             <div class="form-group mb-6">
-                <input type="text" name="domain"
+                <input type="text" name="domain" value="{{ old('domain') }}"
                     class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="domain" placeholder="Domínio da Aplicação">
+                @if ($errors->has('domain'))
+                @foreach ($errors->get('domain') as $message)
+                <span class="text-red-500 font-italic text-xs px-2">{{ $message }}</span>
+                @endforeach
+                @endif
             </div>
             <button type="submit"
                 class="w-full px-6 py-2.5 bg-gray-600 text-white font-semibold leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline- none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out">
@@ -36,7 +47,6 @@ Lista de Ambientes
             </button>
         </form>
     </div>
+</div>
 
-</div>
-</div>
 @endsection
